287ea96ce25bc3fbb0e8743383aa64c1c878439e
zhi_jun <liangzhijun1216@gmail.com>
2024-05-18 21:32:01 -0400

    finished chapter 4

--- build/js/main.js
+++ build/js/main.js
@@ -1,77 +1,70 @@
 "use strict";
-let stringArr = ['one', 'hey', 'dave'];
-let guitars = ['strat', 'les paul', 5150];
-let mixedDate = ['evh', 1984, true];
-stringArr[0] = 'hey';
-stringArr.push('jasmine');
-guitars[0] = 1984;
-// unshift -> add to beginning of array
-guitars.unshift('jim');
-guitars = stringArr;
-mixedDate = guitars;
-let bands = [];
-bands.push('van halen');
-// Tuple 
-// this is strict because it limits how much to be put here
-let myTuple = ['dave', 42, true];
-// union type with string, number, boolean
-let mixed = ['john', 1, false];
-// mixed is a type of array with union array that accepts string number and boolean
-// can't do mytuple = mixed because the target requires 3 elements.cant assign tuples that requires 3 elements 
-mixed = myTuple;
-// Objects
-let myObj;
-myObj = [];
-console.log(typeof myObj);
-myObj = bands;
-const exampleObj = {
-    prop1: 'dave',
-    prop2: true,
+// literaly types
+let myName;
+let userName;
+userName = 'amy';
+// functions
+const add = (a, b) => {
+    return a + b;
 };
-exampleObj.prop1 = 'john';
-// evh is Guitarist type. cannot omit because it gives error
-let evh = {
-    name: 'eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812'],
+const logMsg = (message) => {
+    console.log(message);
 };
-let jp = {
-    name: 'jimmy',
-    active: true,
-    albums: ['I', 'II', 'IV'],
+logMsg('hello');
+logMsg(add(2, 3));
+let subtract = function (c, d) {
+    return c - d;
 };
-// evh and jp are same annotate type
-evh = jp;
-// how to make property optional
-let vh = {
-    // now when i delete a property in here with Guitarist it no have error because 'active' is optional property type
-    name: 'jimmy',
-    albums: ['I', 'II', 'IV'],
+let multiply = function (c, d) {
+    return c * d;
 };
-// both are Guitarist
-evh = jp;
-// make a funation to test out. 
-const greetGuitarist = (guitarist) => {
-    if (guitarist.name) {
-        // if the Guitarist have a name it will return
-        return `hello  ${guitarist.name}!`;
+logMsg(multiply(2, 2));
+// optional parameters comes at last onlu
+const addAll = (a, b, c) => {
+    // if type c is not undefine it will add a + b + c
+    if (typeof c !== 'undefined') {
+        return a + b + c;
     }
-    // if the GUitarist doesn't have a name it will return
-    return 'hello! Missing name';
+    // if type c is undefine it will add a + b
+    return a + b;
 };
-console.log(greetGuitarist(jp));
-// Enums
-/*
-Enums are not a type level addition to JS but something added to the language and runtime
-*/
-// enums start a position 0. if change values, TS will keep in order
-var Grade;
-(function (Grade) {
-    // change values
-    Grade[Grade["U"] = 1] = "U";
-    Grade[Grade["D"] = 2] = "D";
-    Grade[Grade["C"] = 3] = "C";
-    Grade[Grade["B"] = 4] = "B";
-    Grade[Grade["A"] = 5] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.U);
+// assign default values
+const sumAll = (a = 10, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+// to skip value a but it will still have default value
+logMsg(sumAll(undefined, 3));
+// rest parameters. ...nums is for you don't know how many values you put in
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(10, 2, 3));
+// never type
+const createEroor = (errMsg) => {
+    throw new Error(errMsg);
+};
+// endless error so you need a if and else otherwise is endless loop
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+// custom type guard
+const isNumber = (value) => {
+    return typeof value === 'number'
+        ? true : false;
+};
+// to use never type
+const numberOrString = (value) => {
+    if (typeof value === 'string')
+        return 'string';
+    if (isNumber(value))
+        return 'number';
+    return createEroor('this should never happen!');
+};
